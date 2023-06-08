c921b347ffc857930c2f7e06b16fa911bcf1d0ce
diomferreira <diomferreira@gmail.com>
2023-06-08 22:43:09 +0100

    ""

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import PessoaForm
+from django.shortcuts import redirect
 
 # Create your views here.
 @login_required
@@ -11,8 +12,14 @@ def pessoas_list(request):
     return render(request, 'portofolio/pessoal/pessoas/pessoa_list.html')
 
 def pessoas_form(request):
-    form = PessoaForm()
-    return render(request, 'portofolio/pessoal/pessoas/pessoa_form.html', {'form': form})
+    if request.method == "GET":
+        form = PessoaForm()
+        return render(request, 'portofolio/pessoal/pessoas/pessoa_form.html', {'form': form})
+    else:
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/portofolio/pessoas/list/')
 
 def pessoas_delete(request):
     return
