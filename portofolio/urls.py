00cc16a96d63255f795f302f5b67a90f301200b0
diogo <diomferreira@gmail.com>
2023-06-09 13:20:59 +0100

    09/06 CRUD and other CSS

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'portofolio'
 
 urlpatterns = [
-    path('', views.home, name='portfolio-home'),
-    path('pessoas/', views.pessoas_form),
-    path('pessoas/list/', views.pessoas_list),
+    path('', views.home, name='home'),
+    path('pessoas/', views.pessoas_form, name='pessoas_insert'), # get and post req. for insert operation
+    path('pessoas/list/', views.pessoas_list, name='pessoas_list'), # get req. to retrieve and display all records
+    path('pessoas/update/<int:id>', views.pessoas_form, name='pessoa_update'), # get and post req. for update operation
+    path('pessoas/delete/<int:id>', views.pessoas_delete, name='pessoa_delete'),
 ]
--- portofolio/views.py
+++ portofolio/views.py
@@ -2,24 +2,41 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import PessoaForm
 from django.shortcuts import redirect
+from .models import Pessoa
 
 # Create your views here.
 @login_required
 def home(request):
     return render(request, 'portofolio/home.html')
 
+@login_required
 def pessoas_list(request):
-    return render(request, 'portofolio/pessoal/pessoas/pessoa_list.html')
 
-def pessoas_form(request):
+    context = {'pessoas_list': Pessoa.objects.all()}
+
+    return render(request, 'portofolio/pessoal/pessoas/pessoa_list.html', context)
+
+@login_required
+def pessoas_form(request, id=0):
     if request.method == "GET":
-        form = PessoaForm()
+        if id == 0:
+            form = PessoaForm()
+        else:
+            pessoa = Pessoa.objects.get(pk=id)
+            form = PessoaForm(instance=pessoa)
         return render(request, 'portofolio/pessoal/pessoas/pessoa_form.html', {'form': form})
     else:
-        form = PessoaForm(request.POST)
+        if id == 0:
+            form = PessoaForm(request.POST)
+        else:
+            pessoa = Pessoa.objects.get(pk=id)
+            form = PessoaForm(request.POST, instance=pessoa)
         if form.is_valid():
             form.save()
         return redirect('/portofolio/pessoas/list/')
 
-def pessoas_delete(request):
-    return
+@login_required
+def pessoas_delete(request, id):
+    pessoa = Pessoa.objects.get(pk=id)
+    pessoa.delete()
+    return redirect('/portofolio/pessoas/list/')
